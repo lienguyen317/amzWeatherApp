78ab3161a08a7d4f0dbf7566e1cfcf7220bdef82
Lien Nguyen <lien@Liens-MacBook-Air.local>
2020-10-07 22:23:51 -0700

    stateful and remove form component

--- src/components /current/Current.js
+++ src/components /current/Current.js
@@ -1,36 +1,18 @@
 import React from 'react';
 import './current.css';
 
-// function Current(props) {
-//   return (
-//     <div className="Current">
-//       {props.country && props.city && <p>Location: {props.city}, {props.country} </p>}
-//       {props.temperature && <p> Temperature: {props.temperature} </p>}
-//       {props.humidity && <p> Humidity: {props.humidity} </p>}
-//       {props.pressure && <p> Pressure: {props.pressure} </p>}
-//       {props.icon && <img src={`http://openweathermap.org/img/wn/${props.icon}.png`} alt='weather icon'/>}
-//       {props.description && <p> Conditions: {props.description} </p>}
-//       {props.error && <p>{props.error}</p>}
-//     </div>
-//   );
-// }
-
-
-
-
 const Current = (props) => {
   const {data} = props;
 
-  console.log(props)
   return (
     <div>
       <h1>Current Weather</h1>
-      <p>{props.name}</p>
-      <p>{props.sys.country}</p>
-      <p>Temperature: {props.main.temp}</p>
-      <p>Humidity: {props.main.humidity}</p>
-      <p>Pressure: {props.main.pressure}</p>
-      <img src={`http://openweathermap.org/img/wn/${props.icon}.png`} alt='weather icon'/>
+      <p>{data.name}</p>
+      <p>{data.sys.country}</p>
+      <p>Temperature: {data.main.temp}</p>
+      <p>Humidity: {data.main.humidity}</p>
+      <p>Pressure: {data.main.pressure}</p>
+      <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`} alt='weather icon'/>
       <p>Condition: {data.description}</p>
     </div>
   ) 
--- src/components /weather/Weather.js
+++ src/components /weather/Weather.js
@@ -1,69 +1,11 @@
 import React, {useState} from 'react';
 import './weather.css'
-// import Form from '../form/Form.js'
 import Current from '../current/Current.js';
 import Forecast from '../forecast/Forecast.js';
 
 
-// const api_key = '77c638c4087dc16d2d6ba94671248266';
-
-// class Weather extends React.Component {
-//   state ={
-//     temperature:'',
-//     city: '',
-//     humidity:'',
-//     pressure:'',
-//     icon:'',
-//     description:'',
-//     error:'' 
-//   }
-
-//   getWeather = async (e) => {
-//     const city = e.target.elements.city.value;
-//     const country = e.target.elements.country.value;
-//     e.preventDefault();
-//     const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country},$units=imperial&appid=${api_key}`);
-
-//     const response = await api_call.json();
-//     if (city && country) {
-//       this.setState({
-//         temperature: response.main.temp,
-//         city: response.name,
-//         country: response.sys.country,
-//         humidity: response.main.humidity,
-//         pressure: response.main.pressure,
-//         icon: response.weather[0].icon,
-//         description: response.weather[0].description,
-//         error:''
-//       })
-//     } else {
-//       this.setState({error:'Oops!!! Seems like you have forgoten something!'})
-//     }
-//   }
-
-//   render() {
-//     return(
-//       <div>
-//         <Form loadWeather={this.getWeather}/>
-//         <Current  
-//           temperature={this.state.temperature}
-//           city={this.state.city}
-//           country={this.state.country}
-//           humidity={this.state.humidity}
-//           pressure={this.state.pressure}
-//           icon={this.state.icon}
-//           description={this.state.description}
-//           error={this.state.error}
-//         />
-//         <Forecast />
-//       </div> 
-//     )
-//   }
-// }
-
-
 function Weather() {
-const apiKey = '77c638c4087dc16d2d6ba94671248266';
+  const apiKey = '77c638c4087dc16d2d6ba94671248266';
   
   const [form, setForm] = useState ({
     city:'',
@@ -76,17 +18,13 @@ const apiKey = '77c638c4087dc16d2d6ba94671248266';
     if (form.city === ''){
       alert('Add city')
     }else{
-      const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${form.city},${form.country},$units=imperial&appid=${apiKey}`)
+      const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${form.city},${form.country}&units=imperial&appid=${apiKey}`)
         .then((res) => res.json())
-        .then(data => console.log(data))
-
-        setWeather({
-          data: data
-        });
-    }
-  }
-
-  
+        .then((data) => {
+          setWeather({data: data});
+        }
+      )}
+    } 
 
   const handleChange = (e) => {
     let name = e.target.name;
@@ -121,7 +59,6 @@ const apiKey = '77c638c4087dc16d2d6ba94671248266';
       <Forecast />
     </div>
     )
-  
 }
 
   
